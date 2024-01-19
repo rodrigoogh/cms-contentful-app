fa42de429987bd06e30c739586747477c9ef57c1
Rodrigo de Oliveira Gonçalves <rdeoli17@its.jnj.com>
2024-01-19 05:51:35 -0300

    Stackbit changes.

--- app/hero-product.tsx
+++ app/hero-product.tsx
@@ -7,6 +7,7 @@ export interface HeroProductProps {
   image: any;
   description: string;
   category: string[];
+  id: string;
 }
 
 export default function HeroProduct({
@@ -15,23 +16,35 @@ export default function HeroProduct({
   description,
   category,
   slug,
+  id,
 }: HeroProductProps) {
   return (
-    <section>
+    <section data-sb-object-id={id}>
       <div className="mb-8 md:mb-16">
         <CoverImage title={title} slug={slug} url={image.url} />
       </div>
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
         <div>
           <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
-            <Link href={`/product/${slug}`} className="hover:underline">
+            <Link
+              href={`/product/${slug}`}
+              className="hover:underline"
+              data-sb-field-path="title"
+            >
               {title}
             </Link>
           </h3>
-          <div className="mb-4 md:mb-0 text-lg">{category.toString()}</div>
+          <div className="mb-4 md:mb-0 text-lg" data-sb-field-path="category">
+            {category.toString()}
+          </div>
         </div>
         <div>
-          <p className="text-lg leading-relaxed mb-4">{description}</p>
+          <p
+            className="text-lg leading-relaxed mb-4"
+            data-sb-field-path="description"
+          >
+            {description}
+          </p>
         </div>
       </div>
     </section>
